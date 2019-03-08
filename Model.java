377e9089dbb92c64b7b93638a3390a41d458cb36
WanqiLiang <wenkiliang@outlook.com>
2019-03-08 00:09:50 -0500

    update logic

--- Model.java
+++ Model.java
@@ -14,28 +14,24 @@ import java.util.*;
 
 class Model{
     //model.updateLocationAndDirection();
-    //model = new Model(view.getWidth(), view.getHeight()), view.getImageWidth(), view.getImageHeight());
+   
     
-    Direction d = Direction.NORTH;
+    Direction d = Direction.SOUTHWEST;
     
     final int frameWidth;
     final int frameHeight;
     final int imgWidth;
     final int imgHeight ;
-    //ss
+    
     int xloc = 0;
     int yloc = 0;
     final int xIncr = 8; //used as speed basically
     final int yIncr = 6;
     int xChange = xIncr;
     int yChange = yIncr;
-    private static boolean moveEast = false;
-	private static boolean moveWest = false;
-	private static boolean moveNorth = false;
-	private static boolean moveSouth = false;
 
 
-    
+	//model = new Model(view.getWidth(), view.getHeight()), view.getImageWidth(), view.getImageHeight());
     Model(int w, int h, int iw, int ih){
 	frameWidth = w;
 	frameHeight = h;
@@ -55,76 +51,75 @@ class Model{
     	}
     
     public void updateLocationAndDirection(){
-    	if(yloc == 0) {//if reach the upside edge
+
+    	int frameXSize = frameWidth - imgWidth; //adjust for size of image
+    	int frameYSize = frameHeight - imgHeight;
+    	
+    	if(yloc <= 0) {//if reach the upside edge
     		if(d == Direction.NORTH) {//if current direction is north
     			d = Direction.SOUTH;
-    			xloc = xloc; // do nothing w/ x location
-    			yloc += yIncr; //increase the y location
     		}
     		else if(d ==Direction.NORTHEAST) {
     			d = Direction.SOUTHEAST; //if currently moving in south east
-    			xloc += xIncr; // still moving east
-    			yloc += yIncr; //moving south vertically
     		}
     		else if (d == Direction.NORTHWEST) { 
     			d = Direction.SOUTHWEST;
-    			xloc -= xIncr;
-    			yloc += yIncr;
     		}
     	}
-    	else if(yloc == frameHeight) { //if touch the bottom edge
+    	else if(yloc >= frameYSize) { //if touch the bottom edge
     		if(d == Direction.SOUTH) {
     			d = Direction.NORTH;
-    			xloc = xloc;
-    			yloc -= yIncr;
     		}
     		else if(d ==Direction.SOUTHEAST) {
     			d = Direction.NORTHEAST;
-    			xloc += xIncr;
-    			yloc -= yIncr;
     		}
     		else if (d == Direction.SOUTHWEST) {
     			d = Direction.NORTHWEST;
-    			xloc -= xIncr;
-    			yloc -= yIncr;
     		}
     	}
-    	if(xloc == 0) {//if reach the left side edge
+
+    	if(xloc <= 0) {//if reach the left side edge
     		if(d == Direction.WEST) {//if current direction is west
     			d = Direction.EAST; //chance the direction to east
-    			xloc += xIncr; // make it move east
-    			yloc = yloc; //do nothing w/ y location
     		}
     		else if(d ==Direction.SOUTHWEST) {//if currently moving in south west
     			d = Direction.SOUTHEAST; //change the direction to south east
-    			xloc += xIncr; // still moving east
-    			yloc += yIncr; //moving south vertically
     		}
     		else if (d == Direction.NORTHWEST) {  // if moving north west
     			d = Direction.NORTHEAST; //change to north east
-    			xloc += xIncr; //increase x location
-    			yloc -= yIncr; //decrease y location
     		}
     	}
-    	else if(xloc == frameWidth) {//if reach the right side edge
+    	else if(xloc >= frameXSize) {//if reach the right side edge
     		if(d == Direction.EAST) {//if current direction is east
     			d = Direction.WEST; //chance the direction to west
-    			xloc -= xIncr; // make it move west
-    			yloc = yloc; //do nothing w/ y location
     		}
     		else if(d ==Direction.SOUTHEAST) {//if currently moving in south west
     			d = Direction.SOUTHWEST; //change the direction to south east
-    			xloc -= xIncr; // still moving east
-    			yloc += yIncr; //moving south vertically
     		}
     		else if (d == Direction.NORTHEAST) {  // if moving north west
     			d = Direction.NORTHWEST; //change to north east
-    			xloc -= xIncr; //increase x location
-    			yloc -= yIncr; //decrease y location
     		}
     	}
-    	else {
+    	if(d == Direction.NORTH) {
+    		yloc -= yIncr;
+    	}
+    	if(d == Direction.SOUTH) {
+    		yloc += yIncr;
+    	}
+    	if(d == Direction.NORTHEAST) {
     		xloc += xIncr;
+    		yloc -= yIncr;
+    	}
+    	if(d == Direction.NORTHWEST) {
+    		xloc -= xIncr;
+    		yloc -= yIncr;
+    	}
+    	if(d == Direction.SOUTHEAST) {
+    		xloc += xIncr;
+    		yloc += yIncr;
+    	}
+    	if(d == Direction.SOUTHWEST) {
+    		xloc -= xIncr;
     		yloc += yIncr;
     	}
     }
--- View.java
+++ View.java
@@ -1,3 +1,113 @@
+///**
+// * View: Contains everything about graphics and images
+// * Know size of world, which images to load etc
+// *
+// * has methods to
+// * provide boundaries
+// * use proper images for direction
+// * load images for all direction (an image should only be loaded once!!! why?)
+// */
+//import java.awt.Color;
+//import java.awt.Dimension;
+//import java.awt.Graphics;
+//import java.awt.image.BufferedImage;
+//import java.io.*;
+//import java.util.*;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//
+//import javafx.animation.Animation;
+//
+//class View extends JPanel{
+//
+//    final int frameWidth = 500;
+//    final int frameHeight = 300;
+//    final int imgWidth = 165;
+//    final int imgHeight = 165;
+//    private int xloc = 0;
+//	private int yloc = 0;
+//	int xIncr;
+//	int yIncr;
+//    final int frameCount = 10;
+//    int picNum = 0;
+//    BufferedImage[] pics;
+//    BufferedImage[] picsEast;
+//    BufferedImage[] picsNorth;
+//    BufferedImage[] picsSouth;
+//    BufferedImage[] picsSWest;
+//    BufferedImage[] picsSEast;
+//    BufferedImage[] picsNWest;
+//    BufferedImage[] picsNEast;
+//    Direction d = Direction.SOUTH;
+//
+//    //(view.getWidth(), view.getHeight()), view.getImageWidth(), view.getImageHeight());
+//    public int getWidth(){return frameWidth;}
+//    public int getHeight(){return frameHeight;}
+//    public int getImageWidth(){return imgWidth;}
+//    public int getImageHeight(){return imgHeight;}
+//   
+//    
+//    public View() {
+//    	 JFrame frame = new JFrame();
+//    	 frame.setBackground(Color.gray);
+//    	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    		
+//    	 Dimension windowSize = new Dimension(frameWidth, frameHeight);   	
+//    	 frame.setSize(windowSize);
+//    	 frame.setMinimumSize(windowSize);
+//    	 frame.setMaximumSize(windowSize);
+//    		
+//    	 frame.setVisible(true);
+//    	 frame.getContentPane().add(this);
+//    	
+//	BufferedImage img = createImage(d); //for west direction
+//	pics = new BufferedImage[10];
+//	for(int i = 0; i < frameCount; i++)
+//		pics[i]= img.getSubimage(imgWidth*i, 0, imgWidth, imgHeight);
+//}
+//	
+//	
+//    //View.update(model.getX(), model.getY(), model.getDirect()); basically 
+//    public void update(int x, int y, Direction direction){
+//    	xloc = x;
+//    	yloc = y;
+//    	d = direction;
+//    	
+//    	frame.repaint();
+//    	try {
+//    		Thread.sleep(100);
+//    		} catch (InterruptedException e) {
+//    			e.printStackTrace();
+//    			}
+//    	}
+// 
+//    public void paint(Graphics g) {
+//    	picNum = (picNum + 1) % frameCount;
+//    	g.drawImage(pics[picNum],xloc,yloc,Color.gray,this);
+//	}
+//
+//    
+//    //Read image from file and return
+//    private BufferedImage createImage(Direction dir){
+//    	String orcFileString = "images/orc/orc_forward_" + dir.getName() + ".png";
+//    	BufferedImage bufferedImage;
+//    	try {
+//    		bufferedImage = ImageIO.read(new File(orcFileString));
+//    		return bufferedImage;
+//    	} catch (IOException e) {
+//    		e.printStackTrace();
+//    	}
+//    	return null;
+//    	
+//    	// TODO: Change this method so you can load other orc animation bitmaps
+//    }
+//    
+//   
+//    
+//}
+
 /**
  * View: Contains everything about graphics and images
  * Know size of world, which images to load etc
@@ -7,32 +117,37 @@
  * use proper images for direction
  * load images for all direction (an image should only be loaded once!!! why?)
  */
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.*;
-//ss
+
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import javafx.animation.Animation;
 
 class View extends JPanel{
 
-    final int frameWidth = 500;
-    final int frameHeight = 300;
-    final int imgWidth = 165;
-    final int imgHeight = 165;
+    JFrame frame;
+    int frameWidth = 500;
+    int frameHeight = 300;
+    int imgWidth = 165;
+    int imgHeight = 165;
+
     private int xloc = 0;
-	private int yloc = 0;
-	int xIncr;
-	int yIncr;
+    private int yloc = 0;
+    private Direction d;
+
     final int frameCount = 10;
     int picNum = 0;
+
     BufferedImage[] pics;
+    BufferedImage[] picsWest;
     BufferedImage[] picsEast;
     BufferedImage[] picsNorth;
     BufferedImage[] picsSouth;
@@ -40,19 +155,26 @@ class View extends JPanel{
     BufferedImage[] picsSEast;
     BufferedImage[] picsNWest;
     BufferedImage[] picsNEast;
-    Direction d = Direction.SOUTH;
+   
 
+    
     //(view.getWidth(), view.getHeight()), view.getImageWidth(), view.getImageHeight());
     public int getWidth(){return frameWidth;}
     public int getHeight(){return frameHeight;}
     public int getImageWidth(){return imgWidth;}
     public int getImageHeight(){return imgHeight;}
+
+
+    public View() {
+	this.createFrame();
+	this.createImages();
+	pics = picsNWest;
+    }
     
-    //View.update(model.getX(), model.getY(), model.getDirect()); basically 
-    public void update(int x, int y, Direction direction){
-    	JFrame frame = new JFrame();
+    private void createFrame(){
+       
+	frame = new JFrame();
     	frame.getContentPane().add(this);
-    	
     	frame.setBackground(Color.gray);
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	
@@ -62,46 +184,103 @@ class View extends JPanel{
     	frame.setMaximumSize(windowSize);
     	
     	frame.setVisible(true);
-    	
-    	for(int i = 0; i < 1000; i++){
-    		frame.repaint();
-    		try {
-    			Thread.sleep(100);
-    		} catch (InterruptedException e) {
-    			e.printStackTrace();
-    		}
-    		}
-    	xloc = x;
-    	yloc = y;
-    	d = direction;
-    }
-    public void paint(Graphics g) {
-    	picNum = (picNum + 1) % frameCount;
- 
-		g.drawImage(pics[picNum],xloc,yloc,Color.gray,this);
-	}
-    public View() {
-	BufferedImage img = createImage(d); //for west direction
-	pics = new BufferedImage[10];
-	for(int i = 0; i < frameCount; i++)
-		pics[i]= img.getSubimage(imgWidth*i, 0, imgWidth, imgHeight);
-}
     
-    //Read image from file and return
-    private BufferedImage createImage(Direction dir){
-    	String orcFileString = "images/orc/orc_forward_" + dir.getName() + ".png";
+    }
+    
+     private BufferedImage createImage(String d){
     	BufferedImage bufferedImage;
     	try {
-    		bufferedImage = ImageIO.read(new File(orcFileString));
+    		bufferedImage = ImageIO.read(new File("images/orc/orc_forward_" + d +".png"));
     		return bufferedImage;
     	} catch (IOException e) {
     		e.printStackTrace();
     	}
-    	return null;
-    	
-    	// TODO: Change this method so you can load other orc animation bitmaps
+	return null;
+       
     }
-    
+    private void createImages(){
+		
+	BufferedImage img = createImage("west"); //for west direction
+    	picsWest = new BufferedImage[10];
+    	for(int i = 0; i < frameCount; i++)
+    		picsWest[i] = img.getSubimage(imgWidth*i, 0, imgWidth, imgHeight);
+    	
+    	BufferedImage img2 = createImage("east"); //for east direction
+    	picsEast = new BufferedImage[10];
+    	for(int i = 0; i < frameCount; i++)
+    		picsEast[i] = img2.getSubimage(imgWidth*i, 0, imgWidth, imgHeight);
+    	
+    	BufferedImage img3 = createImage("north"); //for north direction
+    	picsNorth = new BufferedImage[10];
+    	for(int i = 0; i < frameCount; i++)
+    		picsNorth[i] = img3.getSubimage(imgWidth*i, 0, imgWidth, imgHeight);
+    	
+    	BufferedImage img4 = createImage("south"); //for south direction
+    	picsSouth = new BufferedImage[10];
+    	for(int i = 0; i < frameCount; i++)
+    		picsSouth[i] = img4.getSubimage(imgWidth*i, 0, imgWidth, imgHeight); 
+    	
+    	BufferedImage img5 = createImage("southwest"); //for south-west direction
+    	picsSWest = new BufferedImage[10];
+    	for(int i = 0; i < frameCount; i++)
+    		picsSWest[i] = img5.getSubimage(imgWidth*i, 0, imgWidth, imgHeight); 
+    	
+    	BufferedImage img6 = createImage("southeast"); //for south-east direction
+    	picsSEast = new BufferedImage[10];
+    	for(int i = 0; i < frameCount; i++)
+    		picsSEast[i] = img6.getSubimage(imgWidth*i, 0, imgWidth, imgHeight); 
+    	
+    	BufferedImage img7 = createImage("northwest"); //for north-west direction
+    	picsNWest = new BufferedImage[10];
+    	for(int i = 0; i < frameCount; i++)
+    		picsNWest[i] = img7.getSubimage(imgWidth*i, 0, imgWidth, imgHeight);
+    }
+
+
+
+    public void paint(Graphics g){
+	picNum = (picNum + 1) % frameCount;
+//	if(d == Direction.NORTH) {
+//	g.drawImage(picsNorth[picNum], xloc, yloc, Color.gray, this); 
+//	}
+	if (d == Direction.EAST) { //draw east
+    	g.drawImage(picsEast[picNum], xloc, yloc, Color.gray, this);
+	}
+	else if (d == Direction.WEST) { //draw west
+    	g.drawImage(picsWest[picNum], xloc, yloc, Color.gray, this);
+	}
+	else if (d == Direction.NORTH) { // draw north
+    	g.drawImage(picsNorth[picNum], xloc, yloc, Color.gray, this);
+	}
+	else if (d == Direction.SOUTH) { // draw south
+    	g.drawImage(picsSouth[picNum], xloc, yloc, Color.gray, this);
+	}
+	else if (d == Direction.SOUTHEAST) { // draw south-east
+    	g.drawImage(picsSEast[picNum], xloc, yloc, Color.gray, this);
+	}
+	else if (d == Direction.SOUTHWEST) { // draw south-west
+    	g.drawImage(picsSWest[picNum], xloc, yloc, Color.gray, this);
+	}
+	else if (d == Direction.NORTHEAST) { // draw north-east
+    	g.drawImage(picsNEast[picNum], xloc, yloc, Color.gray, this);
+	}
+	else if (d == Direction.NORTHWEST) { // draw north-west
+    	g.drawImage(picsNWest[picNum], xloc, yloc, Color.gray, this);
+	}
+    }
    
     
-}
+    //View.update(model.getX(), model.getY(), model.getDirect()); basically 
+    public void update(int x, int y, Direction direction){
+    	xloc = x;
+    	yloc = y;
+    	d = direction;
+    	frame.repaint();
+    	try {
+    		Thread.sleep(100);
+    		} 
+    	catch (InterruptedException e) {
+    		e.printStackTrace();
+    		}
+    	}
+    }
